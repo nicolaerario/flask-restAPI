61bb61de295110dc5fc1d59ec633232ca2060b17
Nicola Erario <nicola.erario@gmail.com>
2019-10-18 21:05:32 +0200

    Added CLI commands and Domains route
    Added some CLI commands to create, drop and seed the db;
    Added tables Marshmallow schema;
    Added domains route to show the domains table content

--- app.py
+++ app.py
@@ -1,14 +1,56 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 # until SQLAlchemy set it to False by default
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+# Database CLI commands utilities
+@app.cli.command("db_create")
+def db_create():
+    db.create_all()
+    print("Database created!")
 
 
+@app.cli.command("db_drop")
+def db_drop():
+    db.drop_all()
+    print("Database dropped!")
+
+
+@app.cli.command("db_seed")
+def db_seed():
+    google = Domain(
+        domain_name="Google.com", domain_type="Search Engine", registered_on="1997"
+    )
+    yahoo = Domain(
+        domain_name="Yahoo.com", domain_type="Internet services", registered_on="1994"
+    )
+    amazon = Domain(
+        domain_name="Amazon.com", domain_type="e-Commerce", registered_on="1994"
+    )
+    db.session.add(google)
+    db.session.add(yahoo)
+    db.session.add(amazon)
+
+    test_user = User(
+        first_name="Paperino",
+        last_name="Paolino",
+        email="paperino@topolinia.com",
+        password="my_str0ngPassword",
+    )
+    db.session.add(test_user)
+    db.session.commit()
+    print("Database seeded!")
+
+
+# App Routes
 @app.route("/")
 def hello_world():
     return jsonify(message="Hello World Flask Rest API"), 200
@@ -19,7 +61,13 @@ def not_found():
     return jsonify(message="Resource not found"), 404
 
 
-# Database models
+@app.route("/domains", methods=["GET"])
+def domains():
+    domains_list = Domain.query.all()
+    return jsonify(domains_schema.dump(domains_list))
+
+
+# Database models and schema
 class User(db.Model):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
@@ -35,3 +83,20 @@ class Domain(db.Model):
     domain_name = Column(String)
     domain_type = Column(String)
     registered_on = Column(String)
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "first_name", "last_name", "email", "password")
+
+
+class DomainSchema(ma.Schema):
+    class Meta:
+        fields = ("domain_id", "domain_name", "domain_type", "registered_on")
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+domain_schema = DomainSchema()
+domains_schema = DomainSchema(many=True)
