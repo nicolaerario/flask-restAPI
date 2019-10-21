732834f9b9c57047fb2c8adcdc42a921dcfb2579
Nicola Erario <nicola.erario@gmail.com>
2019-10-21 19:18:09 +0200

    Added login route
    Added a login route that accepts a POST login request and return a
    JWT access token.
    The login isn't session based but JWT based.
    To manage JWT Flask-JWT-Exended is used.

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
+import os
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 # until SQLAlchemy set it to False by default
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+jwt = JWTManager(app)
+
 
 # Database CLI commands utilities
 @app.cli.command("db_create")
@@ -81,6 +88,23 @@ def register():
         db.session.add(user)
         db.session.commit()
         return jsonify(message="User created successfully!"), 201
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    if request.is_json:
+        email = request.json["email"]
+        password = request.json["password"]
+    else:
+        email = request.form["email"]
+        password = request.form["password"]
+
+    user_match = User.query.filter_by(email=email, password=password).first()
+    if user_match:
+        access_token = create_access_token(identity=email)
+        return jsonify(message="Login succeeded!", access_token=access_token)
+    else:
+        return jsonify(message="Bad email or password!"), 401
 
 
 # Database models and schema
